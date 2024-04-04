1f44ff5f19fb9a2855bbea6388f9d990d762127e
Steven C <lemonkyo2222@outlook.com>
2024-04-04 17:10:51 +0800

    Update message

--- src/agent/player.py
+++ src/agent/player.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Firearm:
     def __init__(self, name: str, distance: float):
         self.Name = name
@@ -8,11 +10,24 @@ class Position:
         self.X = x
         self.Y = y
 
+class Item:
+    def __init__(self, name: str, num: int):
+        self.Name = name
+        self.Num = num
+
+class Inventory:
+    def __init__(self, supplies: List[Item]):
+        self.Supplies = list(supplies)
+
 class Player:
-    def __init__(self, playerId: int, armor: str, health: int, speed: float, firearm: Firearm, position: Position):
+    def __init__(
+        self, playerId: int, armor: str, health: int, speed: float,
+        firearm: Firearm, position: Position, inventory: Inventory
+        ):
         self.PlayerId = playerId
         self.Armor = armor
         self.Health = health
         self.Speed = speed
         self.Firearm = firearm
         self.Position = position
+        self.Inventory = inventory
