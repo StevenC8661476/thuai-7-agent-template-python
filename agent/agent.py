ca80466025b1e6d6eb844ce155a72c55bdc15b42
Andy Zijian Zhang <futrime+git@outlook.com>
2024-05-16 21:48:56 +0800

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -86,13 +86,7 @@ class Agent:
         return self._ws_client.is_connected()
 
     def is_game_ready(self) -> bool:
-        return (
-            self._all_player_info is not None
-            and self._map is not None
-            and self._supplies is not None
-            and self._safe_zone is not None
-            and self._self_id is not None
-        )
+        return self._ticks is not None
 
     async def abandon(self, supply: SupplyKind, count: int):
         logging.debug("%s.abandon(%s, %d)", self, supply, count)
